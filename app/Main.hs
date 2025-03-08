88fd30c2eae86b512f86b50edbd2e64360687918
wagiejack <panatheonofhercules@gmail.com>
2025-03-08 16:06:06 +0530

    did first step

--- app/Main.hs
+++ app/Main.hs
@@ -23,11 +23,11 @@ main = do
   -- You can use print statements as follows for debugging, they'll be visible when running tests.
   hPutStrLn stderr "Logs from your program will appear here"
 
-  -- Uncomment this block to pass stage 1
-  -- if head args /= "-E"
-  --   then do
-  --     putStrLn "Expected first argument to be '-E'"
-  --     exitFailure
-  --   else do if matchPattern pattern input_line
-  --             then exitSuccess
-  --             else exitFailure
+  Uncomment this block to pass stage 1
+  if head args /= "-E"
+    then do
+      putStrLn "Expected first argument to be '-E'"
+      exitFailure
+    else do if matchPattern pattern input_line
+              then exitSuccess
+              else exitFailure
